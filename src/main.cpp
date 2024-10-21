224c2fa4ec01fb863a61a6b6fcc45bd7be9853ee
Mark Kosvin <Mark.Kosvin@leroymerlin.ru>
2024-10-21 11:07:26 +0700

    Set dependency on env variables

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include <algorithm>
 #include <numeric>
 #include <iomanip>
+#include <cmath>
+#include <string>
+#include <cblas.h>
 
 extern "C" {
     void dgetrf_(int*, int*, double*, int*, int*, int*);
@@ -51,7 +54,7 @@ double measure_time(vector<vector<double>>& matrix, int n) {
 
 // Вызов LU через LAPACK
 void LU_decomposition_lapack(vector<double>& matrix, int n) {
-    int* ipiv = new int[n + 1];
+    int* ipiv = new int[n];
     int info;
     dgetrf_(&n, &n, matrix.data(), &n, ipiv, &info);
     delete[] ipiv;
@@ -81,7 +84,7 @@ vector<double> flatten_matrix(const vector<vector<double>>& matrix, int n) {
 }
 
 
-void print_table(const vector<int>& vec1, const vector<double>& vec2, const vector<double>& vec3, 
+void print_table(const bool& run_only_lapack, vector<int>& vec1, const vector<double>& vec2, const vector<double>& vec3, 
                 const vector<double>& vec4, const vector<double>& vec5, const vector<double>& vec6,
                 const vector<double>& vec7, const vector<string>& headers) {
     const int width = 15;
@@ -96,14 +99,22 @@ void print_table(const vector<int>& vec1, const vector<double>& vec2, const vect
     size_t length = vec1.size();
 
     for (size_t i = 0; i < length; ++i) {
-        cout << setw(width) << vec1[i]
-                  << setw(width) << vec2[i]
-                  << setw(width) << vec3[i]
-                  << setw(width) << vec4[i]
-                  << setw(width) << vec5[i]
-                  << setw(width) << vec6[i]
-                  << setw(width) << vec7[i]
-                  << endl;
+        if ( run_only_lapack ){
+            cout << setw(width) << vec1[i]
+                    << setw(width) << vec3[i]
+                    << setw(width) << vec5[i]
+                    << setw(width) << vec7[i]
+                    << endl;
+        } else {
+            cout << setw(width) << vec1[i]
+                    << setw(width) << vec2[i]
+                    << setw(width) << vec3[i]
+                    << setw(width) << vec4[i]
+                    << setw(width) << vec5[i]
+                    << setw(width) << vec6[i]
+                    << setw(width) << vec7[i]
+                    << endl;
+        }
     }
 }
 
@@ -115,28 +126,53 @@ int main() {
         Далее прогоняется через LAPACK
     */
     
-    vector<int> sizes = {4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048}; // размеры матриц
+    bool run_only_lapack = stoi(getenv("RUN_ONLY_LAPACK"));
+    if ( run_only_lapack ) {
+        cout << "Running only LAPACK" << endl;
+    }
+
+    int num_treads_for_lapack = stoi(getenv("SET_THREADS"));
+    openblas_set_num_threads(num_treads_for_lapack);
+    int num_threads = openblas_get_num_threads();
+    cout << "OpenBLAS uses " << num_threads << " threads" << endl;
+
+    int sizes_length = stoi(getenv("SET_SIZES")); // размеры матриц
+    cout << "Sizes of matrix = " << sizes_length << endl;
+    vector<int> sizes(sizes_length); // размеры матриц
+    for (int one_size = 0; one_size < sizes_length; one_size++) {
+        sizes[one_size] = 4 * pow(2, one_size);
+    }
     int sizes_size = sizes.size();
-    int meas_cnt = 3; // сколько измерений за итерацию делать
+    int meas_cnt = stoi(getenv("SET_ITERATIONS")); // сколько измерений за итерацию делать
+    cout << "Iteration per matrix size = " << meas_cnt << endl;
     vector<double> output_min_times(sizes_size);
     vector<double> output_mean_times(sizes_size);
     vector<double> output_max_times(sizes_size);
     vector<double> output_min_times_lpck(sizes_size);
     vector<double> output_mean_times_lpck(sizes_size);
     vector<double> output_max_times_lpck(sizes_size);
-    vector<string> headers = {"Size","Native_min", "LAPACK_min", "Native_mean", "LAPACK_mean", "Native_max", "LAPACK_max"};
+    vector<string> headers;
 
-    for (int one_size = 0; one_size < sizes_size; one_size++) {
-        vector<double> times_vector(meas_cnt);
-        for (int iteration = 0; iteration < meas_cnt; iteration++)  {
-            cout << "Running native. Matrix size: " << sizes[one_size] << " Iteration: " << iteration + 1 << endl;
-            vector<vector<double>> matrix = generate_random_matrix(sizes[one_size]);
-            double time = measure_time(matrix, sizes[one_size]);
-            times_vector[iteration] = time;
+    if ( !run_only_lapack ) {
+        headers = {"Size","Native_min", "LAPACK_min", "Native_mean", "LAPACK_mean", "Native_max", "LAPACK_max"};
+    } else {
+        headers = {"Size", "LAPACK_min", "LAPACK_mean", "LAPACK_max"};
+    }
+
+    if ( !run_only_lapack ) {
+        
+        for (int one_size = 0; one_size < sizes_size; one_size++) {
+            vector<double> times_vector(meas_cnt);
+            for (int iteration = 0; iteration < meas_cnt; iteration++)  {
+                cout << "Running native. Matrix size: " << sizes[one_size] << " Iteration: " << iteration + 1 << endl;
+                vector<vector<double>> matrix = generate_random_matrix(sizes[one_size]);
+                double time = measure_time(matrix, sizes[one_size]);
+                times_vector[iteration] = time;
+            }
+            output_min_times[one_size] = *min_element(times_vector.begin(), times_vector.end());
+            output_max_times[one_size] = *max_element(times_vector.begin(), times_vector.end());
+            output_mean_times[one_size] = (accumulate(times_vector.begin(), times_vector.end(), 0.0)) / static_cast<double>(times_vector.size());
         }
-        output_min_times[one_size] = *min_element(times_vector.begin(), times_vector.end());
-        output_max_times[one_size] = *max_element(times_vector.begin(), times_vector.end());
-        output_mean_times[one_size] = (accumulate(times_vector.begin(), times_vector.end(), 0.0)) / static_cast<double>(times_vector.size());
     }
 
     for (int one_size = 0; one_size < sizes_size; one_size++) {
@@ -153,6 +189,7 @@ int main() {
     }
 
     print_table(
+        run_only_lapack,
         sizes, output_min_times, output_min_times_lpck, 
         output_mean_times, output_mean_times_lpck, output_max_times,
         output_max_times_lpck, headers
